108344d4fb5038fabf7d5a06587ff2f74a939a75
GolovinaElia <eliagolovina@ukr.net>
2021-06-18 23:45:47 +0300

    Добавляет файлы

--- src/App.js
+++ src/App.js
@@ -26,8 +26,19 @@ const App = () => {
         console.log(null);
     }
   };
-  // setTotal((good + 1) + (neutral + 1) + (bad + 1));
-  // setPositivePercentage((Math.round((good * 100) / total)));
+
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  const total = countTotalFeedback();
+
+  const countPositiveFeedbackPercentage = () => {
+    const positivePercentage = Math.round((good * 100) / total);
+    return positivePercentage;
+  };
+  const positivePercentage = countPositiveFeedbackPercentage();
 
   return (
     <Section title="Please leave feedback">
@@ -40,63 +51,11 @@ const App = () => {
         good={good}
         neutral={neutral}
         bad={bad}
-        // total={total}
-        // positivePercentage={positivePercentage}
+        total={total}
+        positivePercentage={positivePercentage}
       />
     </Section>
   );
 };
 
 export default App;
-
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleIncrement = event => {
-//     const name = event.target.name;
-//     this.setState(prevState => ({
-//       [name]: prevState[name] + 1,
-//     }));
-//   };
-
-//   countTotalFeedback = () => {
-//     const { good, neutral, bad } = this.state;
-//     const total = good + neutral + bad;
-//     return total;
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     const { good } = this.state;
-//     const total = this.countTotalFeedback();
-//     const positivePercentage = Math.round((good * 100) / total);
-//     return positivePercentage;
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const total = this.countTotalFeedback();
-//     const positivePercentage = this.countPositiveFeedbackPercentage();
-
-//     return (
-//       <Section title="Please leave feedback">
-//         <FeedbackOptions
-//           options={['good', 'neutral', 'bad']}
-//           onLeaveFeedback={this.handleIncrement}
-//         />
-
-//         <Statistics
-//           good={good}
-//           neutral={neutral}
-//           bad={bad}
-//           total={total}
-//           positivePercentage={positivePercentage}
-//         />
-//       </Section>
-//     );
-//   }
-// }
-// export default App;
